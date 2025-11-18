07ffb57e254e8f2d4e0eaada096daf160b0fb202
b23098 <e1b23098@oit.ac.jp>
2025-11-18 16:04:42 +0900

    DBの追加

---
+++ schedule/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO chamber (userName,chamberName) VALUES ('ほんだ','秘密の小部屋');
+INSERT INTO chamber (userName,chamberName) VALUES ('いがき','1602');
+INSERT INTO chamber (userName,chamberName) VALUES ('ふくやす','マリーナシティ');
+INSERT INTO userinfo (userName,age,height) VALUES ('いがき',30,176.1);
+INSERT INTO userinfo (userName,age,height) VALUES ('ほんだ',18,172.3);
+INSERT INTO userinfo (userName,age,height) VALUES ('ふくやす',NULL,199.9);
+INSERT INTO fruit (name,price) VALUES ('巨峰',1000);
+INSERT INTO fruit (name,price) VALUES ('桃',600);
+INSERT INTO fruit (name,price) VALUES ('なし',200);
+INSERT INTO fruit (name,price) VALUES ('りんご',150);
+INSERT INTO fruit (name,price) VALUES ('かき',180);
+INSERT INTO fruit (name,price) VALUES ('マスカット',550);
+
+/*メールアドレス パスワード 名前*/
