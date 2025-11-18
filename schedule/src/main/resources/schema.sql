f9cf977e4786ba3f65dfcca377cf96ead738f79b
meganedayo <e1b23021@oit.ac.jp>
2025-11-18 16:26:35 +0900

    データベースの初期化

--- schedule/src/main/resources/schema.sql
+++ schedule/src/main/resources/schema.sql
@@ -1,16 +1,6 @@
 /* userやgroupといった名前はSQLでは予約語で使えないため，userNameとしていることに注意 */
-CREATE TABLE chamber (
+CREATE TABLE member (
     id IDENTITY,
     userName VARCHAR NOT NULL,
-    chamberName VARCHAR NOT NULL
-);
-CREATE TABLE userinfo (
-    userName VARCHAR NOT NULL PRIMARY KEY,
-    age INT,
-    height DOUBLE NOT NULL
-);
-CREATE TABLE fruit (
-    id IDENTITY,
-    name VARCHAR NOT NULL,
-    price INT NOT NULL
+    email VARCHAR NOT NULL
 );
